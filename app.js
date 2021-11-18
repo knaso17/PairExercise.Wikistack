46a472c109c2dd87ba736fc6766bd13a73cd7970
China Hoffman <china.hoffman@gmail.com>
2021-11-18 16:03:29 -0500

    defined models and connected to db

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-const express = require('express');
-const morgan = require ('morgan');
-const layout = require ('./views/layout')
-
+const express = require("express");
+const morgan = require("morgan");
+const layout = require("./views/layout");
+const { db, Page, User } = require("./models");
 const app = express();
 
-app.use(express.static('public'));
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
-app.get('/', (req, res) => {
-  res.send(layout(''));
-})
+app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(1337, ()=> {
-  console.log("running on port 1337")
-})
+app.get("/", (req, res) => {
+  res.send(layout(""));
+});
+
+// const init = async () => {
+//   const PORT = 1337;
+//   await Page.sync();
+//   await User.sync();
+//   // make sure that you have a PORT constant
+//   app.listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}!`);
+//   });
+// };
+
+// init();
+
+const init = async () => {
+  const PORT = 1337;
+  await db.sync({});
+  app.listen(1337, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+
+init();
